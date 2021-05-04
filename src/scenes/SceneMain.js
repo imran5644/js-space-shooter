fea2be5149ee9bde0ed415334e8eb60bf5c820be
imran5644 <imi56444@gmail.com>
2021-05-05 03:43:51 +0800

    Amend the animation of the ship

--- src/scenes/SceneMain.js
+++ src/scenes/SceneMain.js
@@ -1,36 +1,52 @@
 import Player from '../entities/Player';
 import GunShip from '../entities/GunShip';
+import ScrollingBackground from '../background/ScrollingBackground';
 
 export default class SceneMain extends Phaser.Scene {
-    constructor() {
-      super({ key: 'SceneMain' });
-    }
-  
-    preload() {
-      this.load.image('background', 'darkPurple.png');
-      this.load.image('enemy', 'enemyBlack2.png');
-      this.load.image('laser', 'laserGreen03.png');
-      this.load.image('myLaser', 'laserRed03.png');
-      this.load.image('playerDamaged', 'playerDamaged.png');
-      this.load.spritesheet('ship', 'ship_spritesheet.png', {
-        frameWidth: 99,
-        frameHeight: 70,
-      });
-    }
-  
-    create() {
-      this.add.image(250, 300, 'background').setScale(3);
-      this.add.image(400, 150, 'enemy');
-      this.add.image(400, 200, 'laser');
-      this.add.image(200, 200, 'myLaser');
-      this.add.image(100, 200, 'playerDamaged');
-      this.physics.add.sprite(100, 450, 'ship');
-    this.anims.create({
-      key: 'ship',
-      frames: this.anims.generateFrameNumbers('ship'),
-      frameRate: 20,
-      repeat: -1,
+  constructor() {
+    super({ key: 'SceneMain' });
+  }
+
+  preload() {
+    this.load.image('background', 'darkPurple.png');
+    this.load.image('enemy', 'enemyBlack2.png');
+    this.load.image('laser', 'laserGreen03.png');
+    this.load.image('myLaser', 'laserRed03.png');
+    this.load.image('playerDamaged', 'playerDamaged.png');
+    this.load.spritesheet('ship', 'ship_spritesheet.png', {
+      frameWidth: 99,
+      frameHeight: 70,
     });
+  }
+
+  create() {
+    // this.add.image(250, 300, 'background').setScale(3);
+    this.anims.create({
+      key: 'left',
+      frames: [{ key: 'ship', frame: 0 }],
+      frameRate: 10,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'ship', frame: 1 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: [{ key: 'ship', frame: 2 }],
+      frameRate: 10,
+      repeat: 0,
+    });
+
+    this.backgrounds = [];
+    for (let i = 0; i < 5; i++) {
+      // create five scrolling backgrounds
+      const bg = new ScrollingBackground(this, 'background', i * 10);
+      this.backgrounds.push(bg);
+    }
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.keySpace = this.input.keyboard.addKey(
@@ -59,7 +75,21 @@ export default class SceneMain extends Phaser.Scene {
             0,
           );
         }
-        
+        // else if (Phaser.Math.Between(0, 10) >= 5) {
+        //   if (this.getEnemiesByType('ChaserShip').length < 5) {
+        //     enemy = new ChaserShip(
+        //       this,
+        //       Phaser.Math.Between(0, this.game.config.width),
+        //       0,
+        //     );
+        //   }
+        // } else {
+        //   enemy = new CarrierShip(
+        //     this,
+        //     Phaser.Math.Between(0, this.game.config.width),
+        //     0,
+        //   );
+        // }
 
         if (enemy !== null) {
           enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
@@ -113,15 +143,26 @@ export default class SceneMain extends Phaser.Scene {
 
     if (!this.player.getData('isDead')) {
       this.player.update();
+
+      if (this.cursors.right.isUp) {
+        this.player.anims.play('turn', true);
+      } else if (this.cursors.left.isUp) {
+        this.player.anims.play('turn', true);
+      }
+
       if (this.cursors.up.isDown) {
         this.player.moveUp();
+        this.player.anims.play('turn', true);
       } else if (this.cursors.down.isDown) {
         this.player.moveDown();
+        this.player.anims.play('turn', true);
       }
       if (this.cursors.left.isDown) {
         this.player.moveLeft();
+        this.player.anims.play('left', true);
       } else if (this.cursors.right.isDown) {
         this.player.moveRight();
+        this.player.anims.play('right', true);
       }
 
       if (this.keySpace.isDown) {
@@ -187,5 +228,5 @@ export default class SceneMain extends Phaser.Scene {
       .getChildren()
       .filter(enemy => enemy.getData('type') == type);
     return enemiesArray;
-    }
   }
+}
