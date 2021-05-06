41fd2cf3793bda8af4a0034106a593bfe19e053e
imran5644 <imi56444@gmail.com>
2021-05-07 01:08:50 +0800

    Create a mock function to mimic the fetchAPI function

--- src/utils/fetchAPI.js
+++ src/utils/fetchAPI.js
@@ -1,8 +1,6 @@
-const fetch = require('node-fetch');
-
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${process.env.GAMEID}/scores`;
 
-const refreshLeaderBoard = async () => {
+const fetchAPI = async () => {
   const resp = await fetch(url, {
     method: 'GET',
     mode: 'cors',
@@ -14,4 +12,4 @@ const refreshLeaderBoard = async () => {
   return data.result;
 };
 
-module.exports = refreshLeaderBoard;
+module.exports = fetchAPI;
